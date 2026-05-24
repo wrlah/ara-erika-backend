a4a8fb9543cde868ad9702160ce4297d2b8a98d4
zetsu <159781505+wrlah@users.noreply.github.com>
2026-05-24 15:38:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,38 +10,89 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
+
 .then(() => console.log("MongoDB connected"))
+
 .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
+
   res.send("Ara Erika Booking Backend Running");
+
 });
 
-// CREATE
+
+
+// ADMIN LOGIN
+
+app.post("/api/admin/login", (req, res) => {
+
+  const { username, password } = req.body;
+
+  if(
+    username === "admin" &&
+    password === "balondobondoc"
+  ){
+
+    res.json({
+      success:true
+    });
+
+  }else{
+
+    res.json({
+      success:false
+    });
+
+  }
+
+});
+
+
+
+// CREATE BOOKING
+
 app.post("/api/bookings", async (req, res) => {
+
   try {
+
     const booking = new Booking(req.body);
+
     await booking.save();
 
     res.status(201).json({
       message: "Booking submitted successfully"
     });
+
   } catch (error) {
+
     res.status(400).json({
       message: "Booking failed",
       error: error.message
     });
+
   }
+
 });
 
-// READ
+
+
+// READ BOOKINGS
+
 app.get("/api/bookings", async (req, res) => {
+
   const bookings = await Booking.find().sort({ createdAt: -1 });
+
   res.json(bookings);
+
 });
 
-// UPDATE
+
+
+// UPDATE BOOKING
+
 app.put("/api/bookings/:id", async (req, res) => {
+
   const updatedBooking = await Booking.findByIdAndUpdate(
     req.params.id,
     req.body,
@@ -49,19 +100,29 @@ app.put("/api/bookings/:id", async (req, res) => {
   );
 
   res.json(updatedBooking);
+
 });
 
-// DELETE
+
+
+// DELETE BOOKING
+
 app.delete("/api/bookings/:id", async (req, res) => {
+
   await Booking.findByIdAndDelete(req.params.id);
 
   res.json({
     message: "Booking deleted"
   });
+
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+
   console.log("Server running on port " + PORT);
+
 });
